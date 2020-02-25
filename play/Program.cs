1b40cd2792e95a236490ff4d876d86b2673e61e4
Chtoucas <policromia@gmail.com>
2020-02-25 07:40:23 +0100

    Project play from F# to C#.
    CheckForOverflowUnderflow = true becomes the default.

---
+++ play/Program.cs
@@ -0,0 +1,14 @@
+﻿// See LICENSE.txt in the project root for license information.
+
+namespace Abc
+{
+    using System;
+
+    class Program
+    {
+        static void Main(/*string[] args*/)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
