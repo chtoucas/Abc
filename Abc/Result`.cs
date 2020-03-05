1ce12d73bdbc11ee0d2e94819f2cf4c307b26069
Chtoucas <policromia@gmail.com>
2020-03-05 12:01:19 +0100

    Play time.

---
+++ Abc/Result`.cs
@@ -0,0 +1,38 @@
+﻿// See LICENSE.txt in the project root for license information.
+
+#pragma warning disable CA1000 // Do not declare static members on generic types
+
+namespace Abc
+{
+    // Both an Option type and a Result type.
+    public abstract class Result<T> where T : notnull
+    {
+        private protected Result() { }
+
+        public static Result<T> None { get; } = new None<T>();
+
+        public static Result<T> Ok(T value) => new Just<T>(value);
+
+        public static Result<T> Error<TError>(TError error) => new Failure<T, TError>(error);
+    }
+
+    public sealed class Just<T> : Result<T> where T : notnull
+    {
+        internal Just(T value) => Value = value;
+
+        public T Value { get; }
+    }
+
+    public class None<T> : Result<T> where T : notnull
+    {
+        internal None() { }
+    }
+
+    // Failure<T> extends None<T> to simplify pattern matching.
+    public sealed class Failure<T, TError> : None<T> where T : notnull
+    {
+        internal Failure(TError error) => Error = error;
+
+        public TError Error { get; }
+    }
+}
--- play/Result`2.cs
+++ play/Result`2.cs
@@ -1,8 +1,20 @@
 ﻿// See LICENSE.txt in the project root for license information.
 
+#pragma warning disable CA1000 // Do not declare static members on generic types
+
 namespace Abc
 {
     using System;
+
+    // Hand-made Either using a ValueTuple.
+    public static class Result<T, TError> where T : notnull
+    {
+        public static (Result<T> success, TError error) Ok(T value)
+            => (Result<T>.Ok(value), default);
+
+        public static (Result<T> success, TError error) Error(TError error)
+            => (Result<T>.None, error);
+    }
 
     // With classes instead of structs, we can use pattern matching.
     // Of course, if we had sum types like in F# it would be simpler and clearer.
@@ -12,7 +24,7 @@ namespace Abc
         public static string ShowOption(bool ok)
             => GetOption(ok) switch
             {
-                Some<int> some => $"{some.Value}",
+                Just<int> some => $"{some.Value}",
                 None<int> _ => "No value",
                 _ => throw new InvalidOperationException()
             };
@@ -20,21 +32,21 @@ namespace Abc
         public static string ShowOutcome(bool ok)
             => GetOutcome(ok) switch
             {
-                Result<int>.Success success => $"{success.Value}",
-                Result<int>.Failure<string> err => err.Error,
+                Just<int> some => $"{some.Value}",
+                Failure<int, string> err => err.Error,
                 _ => throw new InvalidOperationException()
             };
 
         public static string ShowEither(bool ok)
             => GetEither(ok) switch
             {
-                (Some<int> some, _) => $"{some.Value}",
+                (Just<int> some, _) => $"{some.Value}",
                 (None<int> _, string err) => err,
                 _ => throw new InvalidOperationException()
             };
 
         public static Result<int> GetOption(bool ok)
-            => ok ? Result<int>.Some(1) : Result<int>.None;
+            => ok ? Result<int>.Ok(1) : Result<int>.None;
 
         public static Result<int> GetOutcome(bool ok)
             => ok ? Result<int>.Ok(1) : Result<int>.Error("Boum!!!");
