acaec46c642561bf23795f2d6ad9d53f902832ce
Chtoucas <chtoucas@narvalo.org>
2020-03-10 10:28:11 +0100

    TODOs.

--- Abc.Maybe/Maybe.cs
+++ Abc.Maybe/Maybe.cs
@@ -180,6 +180,7 @@ namespace Abc
         {
             if (source is null) { throw new Anexn(nameof(source)); }
 
+            // TODO: optimize when IList<Maybe<T>>.
             foreach (var item in source)
             {
                 if (item.IsSome) { return item; }
--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Features
 --------
 
-- A pure Option type; see project Abc.Maybe.
+- A strict Option type; see project Abc.Maybe.
 - Utilities to write code in the ROP style (Railway Oriented Programming).
 - A fast Gregorian date type?
