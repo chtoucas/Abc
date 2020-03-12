fc9eb7fc61e876b3edf32289e8e7bbdf444b813c
Chtoucas <chtoucas@narvalo.org>
2020-03-12 18:22:18 +0100

    Promote Use().

--- Abc.Maybe/Maybe.cs
+++ Abc.Maybe/Maybe.cs
@@ -59,7 +59,7 @@ namespace Abc
         /// </summary>
         // F# Workflow: return.
         [Pure]
-        public static Maybe<T> Of<T>([AllowNull]T value)
+        public static Maybe<T> Of<T>([AllowNull] T value)
             => value is null ? Maybe<T>.None : new Maybe<T>(value);
 
         /// <summary>
@@ -192,6 +192,35 @@ namespace Abc
         {
             internal static readonly Maybe<IEnumerable<T>> Empty
                 = Of(Enumerable.Empty<T>());
+        }
+    }
+
+    // Extension methods for Maybe<T> where T is disposable.
+    public partial class Maybe
+    {
+        // Bind() with automatic resource cleanup.
+        // F# Workflow: use.
+        [Pure]
+        public static Maybe<TResult> Use<TDisposable, TResult>(
+            this Maybe<TDisposable> @this,
+            Func<TDisposable, Maybe<TResult>> binder)
+            where TDisposable : IDisposable
+        {
+            if (binder is null) { throw new Anexn(nameof(binder)); }
+
+            return @this.Bind(x => { using (x) { return binder(x); } });
+        }
+
+        // Select() with automatic resource cleanup.
+        [Pure]
+        public static Maybe<TResult> Use<TDisposable, TResult>(
+            this Maybe<TDisposable> @this,
+            Func<TDisposable, TResult> selector)
+            where TDisposable : IDisposable
+        {
+            if (selector is null) { throw new Anexn(nameof(selector)); }
+
+            return @this.Select(x => { using (x) { return selector(x); } });
         }
     }
 
--- play/MaybeEx.cs
+++ play/MaybeEx.cs
@@ -198,35 +198,6 @@ namespace Abc
         #endregion
     }
 
-    // Extension methods for Maybe<T> where T is disposable.
-    public partial class MaybeEx
-    {
-        // Bind() with automatic resource cleanup.
-        // F# Workflow: use.
-        [Pure]
-        public static Maybe<TResult> Use<TDisposable, TResult>(
-            this Maybe<TDisposable> @this,
-            Func<TDisposable, Maybe<TResult>> binder)
-            where TDisposable : IDisposable
-        {
-            if (binder is null) { throw new Anexn(nameof(binder)); }
-
-            return @this.Bind(x => { using (x) { return binder(x); } });
-        }
-
-        // Select() with automatic resource cleanup.
-        [Pure]
-        public static Maybe<TResult> Use<TDisposable, TResult>(
-            this Maybe<TDisposable> @this,
-            Func<TDisposable, TResult> selector)
-            where TDisposable : IDisposable
-        {
-            if (selector is null) { throw new Anexn(nameof(selector)); }
-
-            return @this.Select(x => { using (x) { return selector(x); } });
-        }
-    }
-
     // Extensions methods for Maybe<T> where T is an XElement.
     public partial class MaybeEx
     {
