a02ad68138934d643e86e31a4a8195b654e52b24
Chtoucas <chtoucas@narvalo.org>
2020-03-29 17:17:19 +0200

    Split files.

--- Abc.Maybe/Maybe.cs
+++ Abc.Maybe/Maybe.cs
@@ -3,10 +3,8 @@
 namespace Abc
 {
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Diagnostics.Contracts;
-    using System.Linq;
     using System.Runtime.CompilerServices;
 
     using Anexn = System.ArgumentNullException;
@@ -260,42 +258,6 @@ namespace Abc
             return (@this.IsSome && !@this.Value) || (other.IsSome && !other.Value) ? False
                 : @this.IsSome && other.IsSome ? True
                 : Unknown;
-        }
-    }
-
-    // Helpers for Maybe<IEnumerable<T>>.
-    public partial class Maybe
-    {
-        // Obtains the maybe of the empty sequence.
-        // Beware, this is not the same as the empty maybe of type
-        // IEnumerable<T>.
-        [Pure]
-        public static Maybe<IEnumerable<T>> EmptyEnumerable<T>()
-            => MaybeEnumerable_<T>.Empty;
-
-        private static class MaybeEnumerable_<T>
-        {
-            internal static readonly Maybe<IEnumerable<T>> Empty
-                = Of(Enumerable.Empty<T>());
-        }
-    }
-
-    // LINQ extensions for IEnumerable<Maybe<T>> but not extension methods since
-    // they are not for IEnumerable<T>, also to avoid conflicts w/ the standard
-    // LINQ ops.
-    public partial class Maybe
-    {
-        // Filtering: CollectAny (deferred).
-        // Behaviour:
-        // - If the input sequence is empty
-        //     or all maybe's in the input sequence are empty
-        //   returns an empty sequence.
-        // - Otherwise,
-        //   returns the sequence of values.
-        [Pure]
-        public static IEnumerable<T> CollectAny<T>(IEnumerable<Maybe<T>> source)
-        {
-            return from x in source where x.IsSome select x.Value;
         }
     }
 
---
+++ Abc.Maybe/Maybe`1.Async.cs
@@ -0,0 +1,69 @@
+﻿// See LICENSE.txt in the project root for license information.
+
+namespace Abc
+{
+    using System;
+    using System.Diagnostics.Contracts;
+    using System.Threading.Tasks;
+
+    using Anexn = System.ArgumentNullException;
+
+    // Async methods.
+    // NB: these async methods discard the context when they resume.
+    public partial struct Maybe<T>
+    {
+        // TODO: async methods. Check args eagerly. Copy of this. Static AsyncNone.
+        [Pure]
+        public async Task<Maybe<TResult>> BindAsync<TResult>(
+            Func<T, Task<Maybe<TResult>>> binder)
+        {
+            if (binder is null) { throw new Anexn(nameof(binder)); }
+
+            return _isSome ? await binder(_value).ConfigureAwait(false)
+                : Maybe<TResult>.None;
+
+            //var @this = this;
+            //return await __bind(@this).configureawait(false);
+
+            //task<maybe<tresult>> __bind(maybe<t> @this)
+            //    // bonsang! when _issome is true, _value is not null.
+            //    => @this._issome ? binder(@this._value!)
+            //        : task.fromresult(maybe<tresult>.none);
+        }
+
+        [Pure]
+        public async Task<Maybe<TResult>> SelectAsync<TResult>(
+            Func<T, Task<TResult>> selector)
+        {
+            if (selector is null) { throw new Anexn(nameof(selector)); }
+
+            return _isSome ? Maybe.Of(await selector(_value).ConfigureAwait(false))
+                : Maybe<TResult>.None;
+        }
+
+        [Pure]
+        public async Task<Maybe<T>> OrElseAsync(Task<Maybe<T>> other)
+        {
+            if (other is null) { throw new Anexn(nameof(other)); }
+
+            return _isSome ? this : await other.ConfigureAwait(false);
+        }
+
+        // REVIEW: version w/ Func<Task<TResult>>?
+        [Pure]
+        public async Task<TResult> SwitchAsync<TResult>(
+            Func<T, Task<TResult>> caseSome, Task<TResult> caseNone)
+        {
+            if (_isSome)
+            {
+                if (caseSome is null) { throw new Anexn(nameof(caseSome)); }
+                return await caseSome(_value).ConfigureAwait(false);
+            }
+            else
+            {
+                if (caseNone is null) { throw new Anexn(nameof(caseNone)); }
+                return await caseNone.ConfigureAwait(false);
+            }
+        }
+    }
+}
