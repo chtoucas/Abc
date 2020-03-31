46647fa37afc5f599a8456f73867207f7fa2da01
Chtoucas <chtoucas@narvalo.org>
2020-03-31 11:01:06 +0200

    Update qpack.cmd

--- qpack.cmd
+++ qpack.cmd
@@ -9,12 +9,20 @@
 @set Version=1.0.0-alpha-1
 
 :Test
-@call dotnet clean -c Release -v minimal
-@call dotnet test .\Abc.Tests\ %* -c Release --no-restore --nologo
+@call :OnError dotnet clean -c Release -v minimal
+@call :OnError dotnet test .\Abc.Tests\ %* -c Release --no-restore --nologo
 
 :Pack
-@set targetFrameworks=\"netstandard2.0;netstandard2.1;netcoreapp3.1\"
-@call dotnet pack .\Abc.Maybe -c Release --no-build -o __packages --include-symbols -p:TargetFrameworks=%targetFrameworks% -p:PackageVersion=%Version%
+@call :OnError dotnet pack .\Abc.Maybe -c Release --include-symbols ^
+    -o __packages ^
+    -p:TargetFrameworks=\"netstandard2.0;netstandard2.1;netcoreapp3.1\" ^
+    -p:PackageVersion=%Version%
 
 @endlocal
 @exit /b %ERRORLEVEL%
+
+:OnError
+%*
+if errorlevel 1 pause
+@endlocal
+@exit /b 1
