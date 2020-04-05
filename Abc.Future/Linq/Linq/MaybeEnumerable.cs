9a86389c6af086cfbae922aad10bf463674b5628
Chtoucas <policromia@gmail.com>
2020-04-05 09:16:17 +0200

    Explore nested LINQ.

---
+++ Abc.Future/Linq/Linq/MaybeEnumerable.cs
@@ -0,0 +1,43 @@
+﻿// See LICENSE in the project root for license information.
+
+namespace Abc.Linq.Linq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+
+    // Apply standard LINQ ops to an inner enumerable.
+    // Maybe not a good idea. For instance Select() will be confusing: is it
+    // the one from Maybe<T> or the LINQ one.
+    // Of course, it is OK when the signature differs, eg w/ SelectMany().
+    public static partial class MaybeEnumerable { }
+
+    public partial class MaybeEnumerable
+    {
+        [Pure]
+        public static Maybe<IEnumerable<TResult>> Select<T, TResult>(
+            this Maybe<IEnumerable<T>> source,
+            Func<T, TResult> selector)
+        {
+            return source.Select(seq => seq.Select(selector));
+        }
+
+        [Pure]
+        public static Maybe<IEnumerable<T>> Where<T>(
+            this Maybe<IEnumerable<T>> source,
+            Func<T, bool> predicate)
+        {
+            return source.Select(seq => seq.Where(predicate));
+        }
+
+        [Pure]
+        public static Maybe<IEnumerable<TResult>> SelectMany<T, TMiddle, TResult>(
+            this Maybe<IEnumerable<T>> source,
+            Func<T, IEnumerable<TMiddle>> selector,
+            Func<T, TMiddle, TResult> resultSelector)
+        {
+            return source.Select(seq => seq.SelectMany(selector, resultSelector));
+        }
+    }
+}
