9e1f2edfe11f684701a25cef12455522768edcaf
Chtoucas <policromia@gmail.com>
2020-04-06 09:37:16 +0200

    Update code coverage scripts.

--- qcover.ps1
+++ qcover.ps1
@@ -1,12 +1,35 @@
 #Requires -Version 4.0
 
+<#
+.SYNOPSIS
+    Run the Code Coverage script and optionally build human-readable reports.
+.DESCRIPTION
+    Run the Code Coverage script w/ either Coverlet (default) or OpenCover,
+    then optionally build human-readable reports.
+
+    Prerequesites: NuGet packages and tools must have been restored before.
+.PARAMETER OpenCover
+    Use OpenCover instead of Coverlet.
+.PARAMETER NoReport
+    Dot not build HTML/text reports and badges w/ ReportGenerator.
+.INPUTS
+    None.
+.OUTPUTS
+    None.
+.EXAMPLE
+    PS>qcover.ps1
+    Run Coverlet then build the human-readable reports.
+.EXAMPLE
+    PS>qcover.ps1 -x
+    Run OpenCover then build the human-readable reports.
+.EXAMPLE
+    PS>qcover.ps1 -x -n
+    Run OpenCover, do NOT build human-readable reports and badges.
+#>
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory = $false, Position = 0)]
-    [ValidateSet('coverlet', 'opencover')]
-    [Alias('t')] [string] $Tool = 'coverlet',
-
-    [Alias('r')] [switch] $Report
+    [Alias('x')] [switch] $OpenCover,
+    [Alias('n')] [switch] $NoReport
 )
 
 Set-StrictMode -Version Latest
@@ -17,28 +40,62 @@ trap {
     Exit 1
 }
 
-Write-Host "Code Coverage w/ $Tool.`n"
-
 . '.\eng\helpers.ps1'
 
-# Find the versions of OpenCover and ReportGenerator.
-$proj = [Xml] (Get-Content ".\Abc.Tests\Abc.Tests.csproj")
+$artifactsPath = "__"
 
-$openCoverVersion = $proj | Get-ToolVersion -ToolName "OpenCover"
+# Run the Code Coverage tool.
+if ($OpenCover) {
+    $reportType = 'opencover'
+    $reportDir  = "$artifactsPath\$reportType"
+    $reportXml  = "$reportDir\$reportType.xml"
 
-Write-Host $openCoverVersion
+    # Find the OpenCover version.
+    $proj = [Xml] (Get-Content ".\Abc.Tests\Abc.Tests.csproj")
+    $version = $proj | Get-ToolVersion -ToolName "OpenCover"
 
-Exit 0
+    Write-Host "Running OpenCover v$version." -ForegroundColor Green
+    Write-Host "Not done yet." -ForegroundColor Red
+} else {
+    $reportType = 'coverlet'
+    $reportDir  = "$artifactsPath\$reportType"
+    $reportXml  = "$reportDir\$reportType.xml"
 
-# The path is relative to the test project (..\).
-$output = "..\__\$Tool\$Tool.xml"
-$exclude = '\"[Abc*]System.Diagnostics.CodeAnalysis.*,[Abc*]System.Runtime.CompilerServices.*,[Abc*]Microsoft.CodeAnalysis.*\"'
+    Write-Host "Running Coverlet." -ForegroundColor Green
 
-& dotnet test -c Debug --no-restore `
-    /p:CollectCoverage=true `
-    /p:CoverletOutputFormat=opencover `
-    /p:CoverletOutput=$output `
-    /p:Include="[Abc.Maybe]*" `
-    /p:Exclude=$exclude
+    # The path is relative to the test project (..\).
+    $output = "..\$reportXml"
+    $exclude = '\"[Abc*]System.Diagnostics.CodeAnalysis.*,[Abc*]System.Runtime.CompilerServices.*,[Abc*]Microsoft.CodeAnalysis.*\"'
 
-# Write-Host "The report is here: ..." -BackgroundColor 'DarkGreen' -ForegroundColor Yellow
+    & dotnet test -c Debug --no-restore `
+        /p:CollectCoverage=true `
+        /p:CoverletOutputFormat=opencover `
+        /p:CoverletOutput=$output `
+        /p:Include="[Abc.Maybe]*" `
+        /p:Exclude=$exclude
+}
+
+# Build report and badges.
+if ($NoReport) {
+    Write-Host "On user request, no human-readable reports will be generated." `
+        -ForegroundColor Green
+} else {
+    Write-Host "Building HTML/text reports and badge w/ ReportGenerator." `
+        -ForegroundColor Green
+
+    $args = `
+        '-verbosity:Warning',
+        '-reporttypes:HtmlInline;Badges;TextSummary',
+        "-reports:$reportXml",
+        "-targetdir:$reportDir"
+
+    & dotnet tool run reportgenerator $args
+
+    Move-Item -Force -Path "$reportDir\badge_combined.svg" `
+        -Destination "$artifactsPath\$reportType.svg"
+    Move-Item -Force -Path "$reportDir\Summary.txt" `
+        -Destination "$artifactsPath\$reportType.txt"
+
+    Write-Host "The HTML report can be found here: '$reportDir'." `
+        -ForegroundColor Yellow
+}
--- qreport.cmd
+++ qreport.cmd
@@ -5,28 +5,16 @@
 
 @if not exist __\%ReportType% (
     @echo.
-    @echo *** Code Coverage %ReportType% does not exist ***
+    @echo *** File __\%ReportType%\%ReportType%.xml does not exist ***
     @echo.
     @goto ExitOnError
 )
 
-@echo Building report and badges for '%ReportType%'.
+@echo Building HTML report for '%ReportType%'.
 
-@call :OnError dotnet tool run reportgenerator -verbosity:Warning ^
-    -reporttypes:HtmlInline;Badges;TextSummary ^
+@call dotnet tool run reportgenerator -verbosity:Warning ^
+    -reporttypes:HtmlInline ^
     -reports:__\%ReportType%\%ReportType%.xml -targetdir:__\%ReportType%
-
-@move /Y __\%ReportType%\badge_combined.svg __\%ReportType%.svg > nul
-@move /Y __\%ReportType%\Summary.txt __\%ReportType%.txt > nul
 
 @endlocal
 @exit /b %ERRORLEVEL%
-
-:OnError
-%*
-if errorlevel 1 pause
-@goto ExitOnError
-
-:ExitOnError
-@endlocal
-@exit /b 1
