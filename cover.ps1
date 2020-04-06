7d8b59155d43fcc33e2edd0b31d593cad0252057
Chtoucas <chtoucas@narvalo.org>
2020-04-06 13:15:12 +0200

    Cleanup.

--- cover.ps1
+++ cover.ps1
@@ -2,34 +2,37 @@
 
 <#
 .SYNOPSIS
-    Run the Code Coverage script and build human-readable reports.
+Run the Code Coverage script and build human-readable reports.
+
 .DESCRIPTION
-    Run the Code Coverage script w/ either Coverlet (default) or OpenCover, then
-    optionally build human-readable reports and badges.
+Run the Code Coverage script w/ either Coverlet (default) or OpenCover,
+then optionally build human-readable reports and badges.
 
-    Prerequesites: NuGet packages and tools must have been restored before.
+Prerequesites: NuGet packages and tools must have been restored before.
 
-    OpenCover is slow when compared to Coverlet, but we get risk hotspots (NPath
-    complexity, crap score) and the list of unvisited methods. Furthermore, the
-    results differ slightly (LINQ and async so far) which makes the two tools
-    complementary (line counts may differ too but that's a detail).
+OpenCover is slow when compared to Coverlet, but we get risk hotspots
+(NPath complexity, crap score) and the list of unvisited methods.
+Furthermore, the results differ slightly (LINQ and async so far)
+which makes the two tools complementary --- line counts may differ too
+but that's a detail.
+
 .PARAMETER OpenCover
-    Use OpenCover instead of Coverlet.
+Use OpenCover instead of Coverlet.
+
 .PARAMETER NoReport
-    Dot not build HTML/text reports and badges w/ ReportGenerator.
-.INPUTS
-    None.
-.OUTPUTS
-    None.
+Dot not build HTML/text reports and badges w/ ReportGenerator.
+
 .EXAMPLE
-    PS>cover.ps1
-    Run Coverlet then build the human-readable reports.
+PS>cover.ps1
+Run Coverlet then build the human-readable reports.
+
 .EXAMPLE
-    PS>cover.ps1 -x
-    Run OpenCover then build the human-readable reports.
+PS>cover.ps1 -x
+Run OpenCover then build the human-readable reports.
+
 .EXAMPLE
-    PS>cover.ps1 -x -n
-    Run OpenCover, do NOT build human-readable reports and badges.
+PS>cover.ps1 -x -n
+Run OpenCover, do NOT build human-readable reports and badges.
 #>
 [CmdletBinding()]
 param(
@@ -51,45 +54,39 @@ trap {
 # Build warnings MSB3277, the problem is that we then build all platforms
 # within the same dir.
 
-$artifactsDirName = "__"
-$artifactsDir = "$PSScriptRoot\$artifactsDirName"
+$reportType = if ($OpenCover) { 'opencover' } else { 'coverlet' }
+$artifactsDir = "__"
+$outdir = "$PSScriptRoot\$artifactsDir\$reportType"
+$outxml = "$outdir\$reportType.xml"
 
 # Run the Code Coverage tool.
 if ($OpenCover) {
-    $reportType = 'opencover'
-    $reportDir  = "$artifactsDir\$reportType"
-    $reportXml  = "$reportDir\$reportType.xml"
+    Write-Host "Running OpenCover." -ForegroundColor Green
 
     # Find the OpenCover version.
-    $proj = [Xml] (Get-Content ".\Abc.Tests\Abc.Tests.csproj")
-    $version = $proj | Get-ToolVersion -ToolName "OpenCover"
+    $proj = "$PSScriptRoot\Abc.Tests\Abc.Tests.csproj"
+    $version = [Xml] (Get-Content $proj) | Get-ToolVersion -ToolName 'OpenCover'
 
-    if (!(Test-Path $reportDir)) {
-        New-Item -ItemType Directory -Force -Path $reportDir | Out-Null
+    if (!(Test-Path $outdir)) {
+        New-Item -ItemType Directory -Force -Path $outdir | Out-Null
     }
 
-    Write-Host "Running OpenCover v$version." -ForegroundColor Green
-
-    # See https://github.com/opencover/opencover/wiki/Usage
     $openCoverExe = "$env:USERPROFILE\.nuget\packages\opencover\$version\tools\OpenCover.Console.exe"
+    # See https://github.com/opencover/opencover/wiki/Usage
     $filter = "+[Abc.Maybe]* -[Abc]* -[Abc.Future]* -[Abc.Test*]* -[Abc*]System.Diagnostics.CodeAnalysis.* -[Abc*]System.Runtime.CompilerServices.* -[Abc*]Microsoft.CodeAnalysis.*"
-    $targetargs = "test $PSScriptRoot\Abc.Tests\Abc.Tests.csproj -v quiet -c Debug --no-restore /p:DebugType=Full"
+    $targetargs = "test $proj -v quiet -c Debug --no-restore /p:DebugType=Full"
 
     & $openCoverExe -showunvisited -oldStyle -register:user -hideskipped:All `
-        "-output:$reportXml" `
-        "-target:dotnet.exe" `
+        "-output:$outxml" `
+        -target:dotnet.exe `
         "-targetargs:$targetargs" `
         "-filter:$filter" `
-        "-excludebyattribute:*.ExcludeFromCodeCoverageAttribute"
+        -excludebyattribute:*.ExcludeFromCodeCoverageAttribute
 } else {
-    $reportType = 'coverlet'
-    $reportDir  = "$artifactsDir\$reportType"
-    $reportXml  = "$reportDir\$reportType.xml"
-
     Write-Host "Running Coverlet." -ForegroundColor Green
 
     # The path is relative to the test project (..\).
-    $output = "$PSScriptRoot\$artifactsDirName\$reportType\$reportType.xml"
+    $output = "$PSScriptRoot\$artifactsDir\$reportType\$reportType.xml"
     $exclude = '\"[Abc*]System.Diagnostics.CodeAnalysis.*,[Abc*]System.Runtime.CompilerServices.*,[Abc*]Microsoft.CodeAnalysis.*\"'
 
     & dotnet test -c Debug --no-restore `
@@ -111,16 +108,20 @@ if ($NoReport) {
     $args = `
         '-verbosity:Warning',
         '-reporttypes:HtmlInline;Badges;TextSummary',
-        "-reports:$reportXml",
-        "-targetdir:$reportDir"
+        "-reports:$outxml",
+        "-targetdir:$outdir"
 
     & dotnet tool run reportgenerator $args
 
-    Copy-Item -Force -Path "$reportDir\badge_combined.svg" `
-        -Destination "$artifactsDir\$reportType.svg"
-    Copy-Item -Force -Path "$reportDir\Summary.txt" `
-        -Destination "$artifactsDir\$reportType.txt"
+    Push-Location $outdir
 
-    Write-Host "The HTML report can be found here: '$reportDir'." `
+    try {
+        Copy-Item -Force -Path badge_combined.svg -Destination "..\$reportType.svg"
+        Copy-Item -Force -Path Summary.txt -Destination "..\$reportType.txt"
+    } finally {
+        Pop-Location
+    }
+
+    Write-Host "The HTML report can be found here: '$outdir'." `
         -ForegroundColor Yellow
 }
