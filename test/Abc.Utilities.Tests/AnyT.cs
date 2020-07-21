9192eda50887c9faa41b80cca61403377c23ae95
Chtoucas <chtoucas@narvalo.org>
2020-07-21 11:57:55 +0200

    Add a bunch of tests.

---
+++ test/Abc.Utilities.Tests/AnyT.cs
@@ -0,0 +1,12 @@
+﻿// See LICENSE in the project root for license information.
+
+namespace Abc
+{
+    using Abc;
+
+    // AnyT is a plain reference type; no special property whatsoever.
+    internal sealed class AnyT
+    {
+        public AnyT() { }
+    }
+}
---
+++ test/Abc.Utilities.Tests/Utilities/EmptyIteratorTests.cs
@@ -0,0 +1,24 @@
+﻿// See LICENSE in the project root for license information.
+
+namespace Abc.Utilities
+{
+    using System.Collections.Generic;
+
+    using Xunit;
+
+    public static class EmptyIteratorTests
+    {
+        [Fact]
+        public static void Iterate()
+        {
+            // Arrange
+            IEnumerator<AnyT> it = EmptyIterator<AnyT>.Instance;
+            // Act & Assert
+            Assert.False(it.MoveNext());
+            it.Reset();
+            Assert.False(it.MoveNext());
+            it.Dispose();
+            Assert.False(it.MoveNext());
+        }
+    }
+}
