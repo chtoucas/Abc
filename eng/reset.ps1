fbcaa476ee909293da43fae82435cb501d924e99
Chtoucas <policromia@gmail.com>
2020-07-21 10:01:44 +0200

    Create dir "eng".  Add reset script.

---
+++ eng/reset.ps1
@@ -0,0 +1,41 @@
+
+[CmdletBinding()]
+param()
+
+function Remove-BinAndObj {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $path
+    )
+
+    Write-Host "Deleting ""bin"" and ""obj"" directories within ""$path""."
+
+    if (-not (Test-Path $path)) {
+        return Write-Verbose "Skipping ""$path""; the file does NOT exist."
+    }
+    if (-not [System.IO.Path]::IsPathRooted($path)) {
+        return Write-Error "Skipping ""$path""; the path MUST be absolute."
+    }
+
+    ls $path -Include bin,obj -Recurse `
+        | foreach { Write-Verbose "Deleting ""$_""." ; rm $_.FullName -Recurse }
+}
+
+try {
+    $rootdir = (Get-Item $PSScriptRoot).Parent.FullName
+    pushd $rootdir
+
+    Remove-BinAndObj (Join-Path $rootdir "src" -Resolve)
+    Remove-BinAndObj (Join-Path $rootdir "test" -Resolve)
+}
+catch {
+    Write-Host $_ -Foreground Red
+    Write-Host $_.Exception
+    Write-Host $_.ScriptStackTrace
+    exit 1
+}
+finally {
+    popd
+}
