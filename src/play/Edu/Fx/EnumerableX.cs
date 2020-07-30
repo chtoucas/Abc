aa71e9fe17e10ede329a43bcc8d85566cbb72339
Chtoucas <policromia@gmail.com>
2020-07-30 07:22:54 +0200

    Use SPDX-License-Identifier.

--- src/play/Edu/Fx/EnumerableX.cs
+++ src/play/Edu/Fx/EnumerableX.cs
@@ -1,4 +1,5 @@
-﻿// See LICENSE in the project root for license information.
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2019 Narvalo.Org. All rights reserved.
 
 #if NETFRAMEWORK // Enumerable.Append & Enumerable.Prepend
 namespace Abc.Edu.Fx
