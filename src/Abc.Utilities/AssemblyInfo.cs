3462521f17d5668c2032faac79f5e8d41ed02d27
Chtoucas <policromia@gmail.com>
2020-08-01 08:18:19 +0200

    Fix error CS0433.

--- src/Abc.Utilities/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2019 Narvalo.Org. All rights reserved.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Abc.Utilities.Tests" + Abc.AssemblyInfo.PublicKeySuffix)]
