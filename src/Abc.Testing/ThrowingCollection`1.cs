72d439f52555d910328ac978230201b525150272
Chtoucas <chtoucas@narvalo.org>
2020-08-04 13:25:17 +0200

    Create ThrowingCollection`1.cs

---
+++ src/Abc.Testing/ThrowingCollection`1.cs
@@ -0,0 +1,20 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2019 Narvalo.Org. All rights reserved.
+
+namespace Abc
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class to help for deferred execution tests: it throws an exception
+    /// whenever <see cref="GetEnumerator"/> is called.
+    /// </summary>
+    public sealed class ThrowingCollection<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator() => throw new InvalidOperationException();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
